b01fa39c01bafd9e043439c56ac2ebd54c2d6d34
Jose Hernandez <jose.hernandeztorres@yahoo.com>
2018-11-14 17:00:20 -0700

    Initial files

---
+++ assets/js/game.js
@@ -0,0 +1,52 @@
+// Obj to hold questions and answers
+// Narnia themed questions
+var triviaQuestions = [{
+    questions: "Who was the author of the Narnia series?",  // Q1
+    answerList: ["C.J. Parker", "C.S. Lewis", "Don Knotts", "Mary J. Blige", "Robert Ludlum"],
+    answer: 1
+},
+{
+    questions: "Who was the author of the Narnia series?",  // Q2
+    answerList: ["C.J. Parker", "C.S. Lewis", "Don Knotts", "Mary J. Blige", "Robert Ludlum"],
+    answer: 1
+},
+{
+    questions: "Who was the author of the Narnia series?",  // Q3
+    answerList: ["C.J. Parker", "C.S. Lewis", "Don Knotts", "Mary J. Blige", "Robert Ludlum"],
+    answer: 1
+},
+{
+    questions: "Who was the author of the Narnia series?",  // Q4
+    answerList: ["C.J. Parker", "C.S. Lewis", "Don Knotts", "Mary J. Blige", "Robert Ludlum"],
+    answer: 1
+},
+{
+    questions: "Who was the author of the Narnia series?",  // Q5
+    answerList: ["C.J. Parker", "C.S. Lewis", "Don Knotts", "Mary J. Blige", "Robert Ludlum"],
+    answer: 1
+},
+{
+    questions: "Who was the author of the Narnia series?",  // Q6
+    answerList: ["C.J. Parker", "C.S. Lewis", "Don Knotts", "Mary J. Blige", "Robert Ludlum"],
+    answer: 1
+},
+{
+    questions: "Who was the author of the Narnia series?",  // Q7
+    answerList: ["C.J. Parker", "C.S. Lewis", "Don Knotts", "Mary J. Blige", "Robert Ludlum"],
+    answer: 1
+},
+{
+    questions: "Who was the author of the Narnia series?",  // Q8
+    answerList: ["C.J. Parker", "C.S. Lewis", "Don Knotts", "Mary J. Blige", "Robert Ludlum"],
+    answer: 1
+},
+{
+    questions: "Who was the author of the Narnia series?",  // Q9
+    answerList: ["C.J. Parker", "C.S. Lewis", "Don Knotts", "Mary J. Blige", "Robert Ludlum"],
+    answer: 1
+},
+{
+    questions: "Who was the author of the Narnia series?",  // Q10
+    answerList: ["C.J. Parker", "C.S. Lewis", "Don Knotts", "Mary J. Blige", "Robert Ludlum"],
+    answer: 1
+}];
